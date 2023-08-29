4e3db6f28218ba5f3e09106efee3d3bed5d052ff
私はあなたを <707305003@qq.com>
2023-08-29 17:00:16 +0800

    V1.4.7 新增表单设计存在缺陷暂时不显示

--- app/admin/controller/FormDesignController.php
+++ app/admin/controller/FormDesignController.php
@@ -8,8 +8,13 @@ namespace app\admin\controller;
 
 class FormDesignController extends AdminBaseController
 {
+    /**
+     * todo 暂时有问题，影响pjax刷新
+     * @return string
+     * @throws \Exception
+     */
     public function index(): string
     {
-        return  $this->fetch();
+        return $this->fetch();
     }
 }
--- database/seeds/AdminMenus.php
+++ database/seeds/AdminMenus.php
@@ -678,16 +678,17 @@ class AdminMenus extends Seeder
                             ]
                         ]
                     ],
-                    [
-                        "name" => "测试案例（三）",
-                        "url" => "admin/form_design/index",
-                        "icon" => "fas fa-list",
-                        "is_show" => 1,
-                        "is_top" => 0,
-                        "sort_number" => 1000,
-                        "log_method" => "不记录",
-                        "Sons" => []
-                    ]
+                    //todo 测试案例（三）测试未通过
+//                    [
+//                        "name" => "测试案例（三）",
+//                        "url" => "admin/form_design/index",
+//                        "icon" => "fas fa-list",
+//                        "is_show" => 1,
+//                        "is_top" => 0,
+//                        "sort_number" => 1000,
+//                        "log_method" => "不记录",
+//                        "Sons" => []
+//                    ]
                 ]
             ]
         ];
