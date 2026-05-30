3ecde28ea8a20a3840dbfefd6d6863ee79a83e70
shiroi <707305003@qq.com>
2026-05-31 01:53:10 +0800

    Update FileController.php
    Security Fixes
    
    * Fixed insecure client-controlled file_type parameter in file upload endpoint.
    * Added server-side file type whitelist validation.
    * Added dangerous executable extension blacklist validation.
    * Improved upload security to reduce risks associated with unrestricted file uploads.
    * Hardened file validation logic for public upload endpoints.

--- app/common/controller/FileController.php
+++ app/common/controller/FileController.php
@@ -15,52 +15,112 @@ use think\response\Json;
 class FileController extends CommonBaseController
 {
     /**
-     * 公共的文件上传
+     * 公共文件上传
+     *
      * @param Request $request
      * @return Json|void
-     * @example
-     *  - URL : /common/file/upload
-     *  - ACTION: POST
-     *  - FORM_DATA:
-     *     - file(File): xxx.png
      */
     public function upload(Request $request)
     {
-        if ($request->isPost()) {
-            //判断文件是否上传
-            $files = $request->file();
-            if(empty($files)) {
-                return common_error('文件未上传');
-            }
-            $param = $request->param();
-            $field = $param['file_field'] ?? 'file';
-            $dir   = $param['file_dir'] ?? 'uploads';
-            /** @var UploadedFile $file */
-            $file  = $files[$field];
-            // 文件类型，默认图片
-            $file_type = $param['file_type'] ?? get_file_type($file->getOriginalName());
-            // 上传到本地，可自行修改为oss之类的
-            $config = config('filesystem.disks.public');
-
-            try {
-                validate([$field => $config['validate'][$file_type]])->check($files);
-            } catch (ValidateException $e) {
-                return common_error($e->getMessage());
-            }
-            $name = Filesystem::putFile($dir, $file);
-
-            $url = $config['url'] . '/' . $name;
-            $url = str_replace("\\", '/', $url);
-
-            return common_success([
-                'url'         => $url,
-                'name'        => str_replace("\\", '/', $file->getOriginalName()),
-                'size'        => $file->getSize(),
-                'type'        => $file_type,
-                'mime'        => $file->getMime()
-            ]);
+        if (!$request->isPost()) {
+            return common_error('非法访问');
         }
 
-        return common_error('非法访问');
+        $files = $request->file();
+
+        if (empty($files)) {
+            return common_error('文件未上传');
+        }
+
+        $param = $request->param();
+
+        $field = $param['file_field'] ?? 'file';
+        $dir   = $param['file_dir'] ?? 'uploads';
+
+        if (!isset($files[$field])) {
+            return common_error('上传字段不存在');
+        }
+
+        /** @var UploadedFile $file */
+        $file = $files[$field];
+
+        $config = config('filesystem.disks.public');
+
+        /**
+         * 服务端自动识别文件类型
+         * 禁止客户端传入 file_type
+         */
+        $fileType = get_file_type($file->getOriginalName());
+
+        /**
+         * 白名单校验
+         */
+        $allowTypes = ['image', 'video', 'file'];
+
+        if (!in_array($fileType, $allowTypes, true)) {
+            return common_error('不支持的文件类型');
+        }
+
+        if (!isset($config['validate'][$fileType])) {
+            return common_error('文件校验规则不存在');
+        }
+
+        /**
+         * 危险扩展名黑名单
+         */
+        $denyExt = [
+            'php',
+            'php3',
+            'php4',
+            'php5',
+            'phtml',
+            'phar',
+            'cgi',
+            'pl',
+            'py',
+            'jsp',
+            'jspx',
+            'asp',
+            'aspx',
+            'sh',
+            'bash',
+            'exe',
+            'dll',
+            'com',
+            'bat',
+            'cmd'
+        ];
+
+        $ext = strtolower($file->extension());
+
+        if (in_array($ext, $denyExt, true)) {
+            return common_error('禁止上传此类型文件');
+        }
+
+        try {
+            validate([
+                $field => $config['validate'][$fileType]
+            ])->check([
+                $field => $file
+            ]);
+        } catch (ValidateException $e) {
+            return common_error($e->getMessage());
+        }
+
+        $name = Filesystem::putFile($dir, $file);
+
+        $url = str_replace(
+            '\\',
+            '/',
+            $config['url'] . '/' . $name
+        );
+
+        return common_success([
+            'url'  => $url,
+            'name' => $file->getOriginalName(),
+            'size' => $file->getSize(),
+            'type' => $fileType,
+            'mime' => $file->getMime(),
+        ]);
     }
 }
